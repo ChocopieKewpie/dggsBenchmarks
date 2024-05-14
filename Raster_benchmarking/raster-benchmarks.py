927632a364f0a7087112e33147e0e01d91304749
alpha-beta-soup <richard.m.law@gmail.com>
2024-05-15 11:54:40 +1200

    refactoring raster h3 indexing

--- Raster_benchmarking/raster-benchmarks.py
+++ Raster_benchmarking/raster-benchmarks.py
@@ -16,7 +16,7 @@ raster_metadata = lambda cellSize: dict(
         cellSize=cellSize
     )
 
-layers = 500
+layers = 10000
 
 # Discrete
 for layer in range(0,layers):
@@ -24,12 +24,12 @@ for layer in range(0,layers):
     nlm = nlmpy.mpd(nRow, nCol, 0.75)
     # Classified midpoint displacement NLM
     nlm = nlmpy.classifyArray(nlm, np.random.random_integers(1,10,(1,10))).astype(int)
-    nlmpy.exportASCIIGrid(f'/home/lawr/dggs-benchmarks/discrete/raster-{layer}.asc', nlm, **raster_metadata(10))
+    nlmpy.exportASCIIGrid(f'./discrete/raster-{layer}.asc', nlm, **raster_metadata(10))
 
 # Continuous
 for layer in range(0,layers):
     # Midpoint displacement NLM
     nlm = nlmpy.mpd(nRow, nCol, 0.75)
-    nlmpy.exportASCIIGrid(f'/home/lawr/dggs-benchmarks/continuous/raster-{layer}.asc', nlm, **raster_metadata(10))
+    nlmpy.exportASCIIGrid(f'./continuous/raster-{layer}.asc', nlm, **raster_metadata(10))
 
 sys.exit(0)
